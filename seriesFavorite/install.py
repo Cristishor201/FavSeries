6f51fd1e9599063c002cb63446f3c6c135a49b8b
Cristishor201 <cristif92@gmail.com>
2020-08-13 09:25:43 +0300

    adding installing modules

--- seriesFavorite/install.py
+++ seriesFavorite/install.py
@@ -1,4 +1,5 @@
 import os, re, pyautogui
+import subprocess, sys
 
 # edit the abs path of saving
 path = os.getcwd()
@@ -11,8 +12,18 @@ with open("seriesFavorite.py", "r") as r:
 with open("seriesFavorite.py", "w") as w:
     w.write('\n'.join(lines[:4] + [new_line] + lines[5:]))
 
+# create bat file
 response = pyautogui.confirm(title="Bat file", text="Do you want to create a bat file and for running the script at every start up ?", buttons=["YES", "No"])
 if response == "YES":
     with open("startUp.bat", 'w') as s:
         text = "@py " + path + "\seriesFavorite.py\n@pause"
         s.write(text)
+
+# install required modules
+subprocess.check_call([sys.executable, '-m', 'pip', 'install', 'requests'])
+subprocess.check_call([sys.executable, '-m', 'pip', 'install', 'pyautogui'])
+subprocess.check_call([sys.executable, '-m', 'pip', 'install', 'beautifulsoup4'])
+subprocess.check_call([sys.executable, '-m', 'pip', 'install', 'pywin32'])
+
+# put the shorcut in startUp
+import win32com.client
