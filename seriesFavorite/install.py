ac13cda55e9b9d77061f39b0ed87ed3dcf20338a
Cristian Florescu <cristif92@gmail.com>
2020-08-19 21:05:09 +0300

    finished the install.py

--- seriesFavorite/install.py
+++ seriesFavorite/install.py
@@ -6,11 +6,12 @@ path = os.getcwd()
 with open("seriesFavorite.py", "r") as r:
     lines = r.read().split('\n')
     line = lines[4]
-    parts = re.split("([\('\)]+)", line)
-    new_line = ''.join(parts[:2] + [path] + parts[3:])
+    parts = re.split('([\("\)]+)', line)
+    new_line = ''.join(parts[:2] + [path] + parts[3:]).replace("\\", "/")
 
 with open("seriesFavorite.py", "w") as w:
-    w.write('\n'.join(lines[:4] + [new_line] + lines[5:]))
+    w.write('\n'.join(lines[:4] + [] + lines[5:]))
+    print("<", new_line , ">")
 
 # install required modules
 subprocess.check_call([sys.executable, '-m', 'pip', 'install', 'requests'])
@@ -21,9 +22,6 @@ subprocess.check_call([sys.executable, '-m', 'pip', 'install', 'pywin32'])
 # create bat file
 response = pyautogui.confirm(title="Bat file", text="Do you want to create a bat file and for running the script at every start up ?", buttons=["YES", "No"])
 if response == "YES":
-    with open("../startUp.bat", 'w') as s:
+    with open("startUp.bat", 'w') as s:
         text = "@py " + path + "\seriesFavorite.py\n@pause"
         s.write(text)
-
-# put the shorcut in startUp
-import win32com.client
