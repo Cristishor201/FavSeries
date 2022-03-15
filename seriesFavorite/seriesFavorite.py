5656a4c7e4208a5cc2c258b447219d9b05c50c6f
Cristian Florescu <cristif92@gmail.com>
2022-03-15 21:00:47 +0200

    1 Added, 1 Fixed
    Improved scalability
    Started working on the scroll bar (not finished)
    organised selectors
    improved speed (less code)

--- seriesFavorite/seriesFavorite.py
+++ seriesFavorite/seriesFavorite.py
@@ -1,4 +1,5 @@
 import os, shelve, requests, webbrowser, bs4, re, time, pyautogui, logging
+#from tkinter import ttk, Tk, Label, Scale, HORIZONTAL, Button
 from tkinter import *
 
 # Change the folder of saving
@@ -20,10 +21,13 @@ class Movie:
     - incomplete link of a season
     """
 
-    def __init__(self, title, url, selectors):
+    def __init__(self, title, main_url, selectors): # selector=[ serial, episode]
         # take season, episode In
         self.title = title
+        self.main_url = main_url
+        self.selectors = selectors
         self.shelvF = shelve.open('series')
+
         if self.title in list(self.shelvF.keys()):
             self.seasonIn, self.episodeIn = self.shelvF[self.title]
         else:
@@ -32,52 +36,58 @@ class Movie:
             self.shelvF[title] = [self.seasonIn, self.episodeIn]
             logging.error("2", self.shelvF[title])
 
-        if self.seasonIn != 0:
-            # link sesons -> take seasonMax
-            seasonRes = requests.get(url)
-            seasonRes.raise_for_status()
-            selectAllTitles = selectors[0]
-            page = bs4.BeautifulSoup(seasonRes.text, 'html.parser') # seasons
-            elements = page.select(selectAllTitles) # list of all elems
-            text = elements[-1].text
-            seasonMax = int(text[-2:].strip())
-            self.seasonMax = seasonMax
-        else:
-            self.seasonMax = 0
 
-        #  for -> for - save all the links in list finding
-        num = 0 ; first = 0 ; self.links = []
-        selectAllEpsodes = selectors[1]
-        for season in range(self.seasonIn, self.seasonMax+1):
-            ep = [] # list of episodes found
-            if self.seasonIn !=0:
-                episodeRes = requests.get(selectors[2] + str(season) + '/')
+    @staticmethod
+    def arr_start(arr_object): # [Boruto, Naruto, Flash]
+        for item in arr_object:
+            main_url = item.get_main_url() ; selectors = item.get_selectors()
+            episodeIn = item.get_episodeIn() ; forWatching = []
+            if len(item.selectors) == 2 :
+                seasonIn = item.get_seasonIn() ;
+                links = item.crawl(main_url, selectors[0])
+                seasonMax = len(links)
+                first = True
+                for i in range(seasonIn-1, seasonMax):
+                    if first:
+                        episode = item.crawl(links[i], selectors[1])[::-1][(episodeIn-1):]
+                        first = False
+                    else:
+                        episode = item.crawl(links[i], selectors[1])[::-1]
+                    forWatching.extend(episode)
+                item.set_links(forWatching) ; item.set_num(len(forWatching))
+
+            elif len(item.selectors) == 1:
+                episode = item.crawl(main_url, selectors[0])[::-1][(episodeIn +1):-1]
+                forWatching.extend(episode)
+                item.set_links(forWatching) ; item.set_num(len(forWatching))
+
             else:
-                episodeRes = requests.get(url)
-            episodeRes.raise_for_status()
-            page = bs4.BeautifulSoup(episodeRes.text, 'html.parser')
-            elements = page.select(selectAllEpsodes)
+                raise Exception("Error - too more or none selectors")
 
-            regexUrl = re.compile(r'''
-            # https://divxfilmeonline.org/the-flash-sezonul-5-episodul-13/
-            (href=")    #start part
-            ([\w:/.-]+)
-            (")         # end part
-            ''', re.VERBOSE)
-            if(first == 0):
-                begin = self.episodeIn -1 # substract episodes viewed
-                first += 1
-            else:
-                begin = 0
+    @staticmethod
+    def crawl(url, selector):
+        result = []
+        res = requests.get(url)
+        res.raise_for_status()
+        selectAllLink = selector
+        page = bs4.BeautifulSoup(res.text, 'html.parser')
+        elements = page.select(selectAllLink)
+        regexUrl = re.compile(r'''
+        # https://divxfilmeonline.org/the-flash-sezonul-5-episodul-13/
+        (href=")    #start part
+        ([\w:/.-]+)
+        (")         # end part
+        ''', re.VERBOSE)
+        for i in range(len(elements)):
+            url = regexUrl.search(str(elements[i]))
+            result.append(url.group(2))
+        return result
 
-            for episode in range(0, len(elements) - begin):
-                url = regexUrl.search(str(elements[episode]))
-                num += 1
-                ep.insert(0, url.group(2)) # per episode
-
-            self.links.extend(ep) # add season of episode links
+    def set_num(self, num):
         self.num = num
 
+    def set_links(self, links):
+        self.links = links
 
     def _reset(self):
         print("[seasonIn, episodeIn]", [self.seasonIn, self.episodeIn])
@@ -116,6 +126,18 @@ class Movie:
         logging.critical("6", self.seasonIn, self.episodeIn)
         self.shelvF[self.title] = [self.seasonIn, self.episodeIn]
 
+    def get_main_url(self):
+        return self.main_url
+
+    def get_selectors(self):
+        return self.selectors
+
+    def get_seasonIn(self):
+        return self.seasonIn
+
+    def get_episodeIn(self):
+        return self.episodeIn
+
     def __del__(self):
         self.shelvF.close()
 
@@ -124,40 +146,73 @@ class UI:
 
     def __init__(self, window, movies): # user interface
         self.movies = movies
+        self.sliders = []
+        self.window = window
 
-        window = window
         window.wm_title(" Filme noi:")
         window.iconbitmap('img/movies.ico')
-        window.geometry('370x200')
+        window.geometry('370x220')
 
-        Label1 = Label(window, text=movies[0].title, font=("Helvetica", 13))
-        Label2 = Label(window, text=movies[1].title, font=("Helvetica", 13))
-        Label3 = Label(window, text=movies[2].title, font=("Helvetica", 13))
+        frame1 = Frame(window, width=370)
+        frame2= Frame(window, height=50)
 
-        self.Slider1 = Scale(window, from_=0, to=movies[0].num, orient=HORIZONTAL, length=250, state=["disabled" if movies[0].num == 0 else "active"])
-        self.Slider2 = Scale(window, from_=0, to=movies[1].num, orient=HORIZONTAL, length=250, state=["disabled" if movies[1].num == 0 else "active"])
-        self.Slider3 = Scale(window, from_=0, to=movies[2].num, orient=HORIZONTAL, length=250, state=["disabled" if movies[2].num == 0 else "active"])
-        self.Slider1.set(movies[0].num)
-        self.Slider2.set(movies[1].num)
-        self.Slider3.set(movies[2].num)
+        window.grid_rowconfigure(0, weight=3)
+        window.grid_rowconfigure(1, weight=1)
+        frame2.grid_columnconfigure(1, weight=1)
 
-        Button1 = Button(window, text="CLOSE", width=6, command=window.destroy)
-        Button2 = Button(window, text="OPEN", width=7, command=self.opening)
+        frame1.grid(row=0, sticky="nsew")
+        frame2.grid(row=1, sticky="nsew")
 
-        Label1.grid(row=0, column=0, pady=(10,0), padx=(8,8))
-        Label2.grid(row=1, column=0, pady=(10,0), padx=(8,8))
-        Label3.grid(row=2, column=0, pady=(10,0), padx=(8,8))
-        self.Slider1.grid(row=0, column=1, columnspan=3, pady=(0,5))
-        self.Slider2.grid(row=1, column=1, columnspan=3, pady=(0,5))
-        self.Slider3.grid(row=2, column=1, columnspan=3, pady=(0,5))
-        Button1.grid(row=3, column=0, padx=(10,0), pady=15)
-        Button2.grid(row=3, column=3, padx=(0,5), pady=15)
+        #scroll
+        mycanvas = Canvas(frame1, bg="blue")
+        mycanvas.grid_rowconfigure(0, weight=1)
+        mycanvas.grid(row=0, column=0, sticky="nsew")
 
-    def opening(self): # open selected movie
-        s = [self.Slider1.get(), self.Slider2.get(), self.Slider3.get()]
-        if s == [0, 0, 0]:
+        myScrollBar = Scrollbar(frame1, orient=VERTICAL, command=mycanvas.yview)
+        myScrollBar.grid(row= 0, column= 0)
+
+        mycanvas.configure(yscrollcommand=myScrollBar.set)
+        mycanvas.bind('<Configure>', lambda e: mycanvas.configure(scrollregion=mycanvas.bbox("all")))
+
+        frame_scrollable = Frame(mycanvas)
+
+        mycanvas.create_window((0,0), window=frame_scrollable, anchor="nw")
+
+        if len(movies) > 3:
+            print("scroll")
+            #main_window(window) #schimb self.window
+        else:
+            print("normal")
+
+        for i in range(len(movies)):
+            Labelt = Label(frame_scrollable, text=movies[i].title, font=("Helvetica", 13))
+            Labelt.grid(row=i, column=0, pady=(10,0), padx=(8,8))
+
+            Slider = Scale(frame_scrollable, from_=0, to=movies[i].num, orient=HORIZONTAL, length=250, state=["disabled" if movies[i].num == 0 else "active"])
+            Slider.set(movies[i].num)
+            Slider.grid(row=i, column=1, columnspan=3, pady=(0,5))
+            self.sliders.append(Slider)
+
+        Button1 = Button(frame2, text="CLOSE", width=6, command=window.destroy)
+        Button2 = Button(frame2, text="OPEN", width=7, command=self.opening)
+
+        Button1.grid(row=0, column=0, padx=(25,0), pady=15, sticky="w")
+        Button2.grid(row=0, column=3, padx=(0,35), pady=15, sticky="e")
+        
+    def opening(self): ######### open selected movie
+        s = [] # values with selected in sliders
+        count = 0
+        for i in range(len(self.sliders)):
+            item = self.sliders[i].get()
+            if item == 0:
+                count+=1
+            s.append(item)
+
+        if count == i + 1: # if all are zeros exit
+            print("all are zeros")
             return False
         window.destroy()
+
         for i in range(len(self.movies)):
             for j in range(s[i]):
                 webbrowser.open(self.movies[i].links[j], new=0) # open how many movies I want to see once
@@ -167,27 +222,18 @@ class UI:
             for item, num in zip(self.movies, s):
                 logging.info("3", item, num)
                 item.update(num)
-        else:
-            pass
 
 if __name__ == '__main__':
-    Boruto = Movie('Boruto',
-    'https://narutotubes.com/boruto-naruto-next-generations-eng-sub/',
-    ["___",
-    '#post-2579 > .entry-content > ul.display-posts-listing > .listing-item a',
-    "___"]
-    )
-    Flash = Movie('Flash',
+    Boruto = Movie('Boruto', 'https://www.naruget.tv/category/boruto-subbed/', ["div.c50 > div.eptit > a",])
+
+    """Flash = Movie('Flash',
     'https://divxfilmeonline.org/seriale-online/the-flash/',
-    ['ul.listing-cat span', # for getting season max
-    '#content > ul.listing-videos.listing-tube > li a', # select all episodes
-    'https://divxfilmeonline.org/seriale-online/the-flash/the-flash-sezonul-']) # link per season
-    SuperGirl = Movie('SuperGirl',
-    "https://divxfilmeonline.org/seriale-online/supergirl/",
-    ['ul.listing-cat span',
-    '#content > ul.listing-videos.listing-tube > li a',
-    'https://divxfilmeonline.org/seriale-online/supergirl/supergirl-sezonul-'])
-    videos=[Boruto, Flash, SuperGirl]
+    ["li.border-radius-5 a",
+    "ul.listing-videos li a"])
+    """
+
+    videos=[Boruto, Boruto, Boruto, Boruto, Boruto]
+    Movie.arr_start(videos)
 
     window = Tk()
     ui = UI(window, videos)
